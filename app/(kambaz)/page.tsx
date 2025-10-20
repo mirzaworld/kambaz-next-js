beaf461b8d5868a7190aedc94a8943cd4f0ea5cb
mirzaworld <baig.mirzas@northeastern.edu>
2025-10-20 15:54:15 -0400

    a3-redirect

--- app/(kambaz)/page.tsx
+++ app/(kambaz)/page.tsx
@@ -1,9 +1,5 @@
-import Link from "next/link";
-import { redirect } from "next/navigation";
-
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-static'; 
 
 export default function kambaz() {
-  redirect("/account/signin");
   return null;
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  async redirects() {
+    return [
+      {
+        source: '/',              
+        destination: '/account/signin',
+        permanent: false,  
+      },        
+    ];
+  },
 };
-
 export default nextConfig;
