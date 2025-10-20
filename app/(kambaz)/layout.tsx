9018eec9b3952a1c90bf2cda3126e4bb0c598df2
mirzaworld <baig.mirzas@northeastern.edu>
2025-10-20 15:04:37 -0400

    wrappingforsearchparams

--- app/(kambaz)/layout.tsx
+++ app/(kambaz)/layout.tsx
@@ -1,18 +1,20 @@
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import KambazNavigation from "./navigation";
 import "./styles.css";
 
-export default function KambazLayout({ children }: Readonly<{ children : ReactNode}>) {
-    return (
-        <div id = "wd-kambaz">
-            <div className = "d-flex" >
-                <div>
-                    <KambazNavigation />
-                </div>
-                <div className = "wd-main-content-offset p-3 flex-fill" >
-                    { children }
-                </div>
-            </div>
+export default function KambazLayout({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <div id="wd-kambaz">
+      <div className="d-flex">
+        <div>
+          <Suspense fallback={null}>
+            <KambazNavigation />
+          </Suspense>
         </div>
-    );
+        <div className="wd-main-content-offset p-3 flex-fill">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
 }
