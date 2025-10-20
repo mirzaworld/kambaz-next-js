67db34ebe9f157c41f3abf72bd6e4d9693fadc1a
mirzaworld <baig.mirzas@northeastern.edu>
2025-10-20 19:46:03 -0400

    Revert "a3-build-error-fix"
    This reverts commit fc520084b1101f6d370a6b672bda1d2c31a49409.

--- app/(kambaz)/page.tsx
+++ app/(kambaz)/page.tsx
@@ -1,9 +1,7 @@
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
-export const dynamic = "force-dynamic";
-
 export default function kambaz() {
-  redirect("/account/signin");
-  return null;
+    redirect ("/account/signin");
+    return null;
 }
